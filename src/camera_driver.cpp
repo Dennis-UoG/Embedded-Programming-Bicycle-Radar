f87da4252dfae78edac28768e11de1b142931cb5
yuehuaz424 <2992646Z@student.gla.ac.uk>
2025-03-18 12:44:30 +0800

    bug fixes

--- src/camera_driver.cpp
+++ src/camera_driver.cpp
@@ -138,12 +138,6 @@ void clearFolder(const std::string& folderPath) {
 
 int CameraMainThread() {
     std::cout << "Camera Manager started." << std::endl;
-<<<<<<< HEAD
-    
-    clearFolder(SAVE_FOLDER_PATH);
-    
-=======
->>>>>>> 87d3cfec8802e974147d2a914aafb5a23bfa581e
 
     clearFolder(SAVE_FOLDER_PATH);
     
